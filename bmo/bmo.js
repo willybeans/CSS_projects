e797dc39a1117a6de3ba95cf533a91f212662e04
willybeans <willwedmedyk@gmail.com>
2018-04-03 13:37:24 -0400

    issues to fix:  button sizing, '.' is incorrect

--- bmo/bmo.js
+++ bmo/bmo.js
@@ -1,8 +1,14 @@
+/* issues to be adddressed:
+- the "." behavior is incorrect
+- the button sizing is inconsistent
+*/
+
 var buttons = ['CE','AC', '*','7','8','9','/','4','5','6',
                '-','1','2','3','+','0','.','='];
 var currentEntry = [], totalEntry = [];
 var testNum = /[0-9]/g;
 var testOperands = /[+\-\/*=]/;
+var equals = true;
 
 window.onload = () => {
   makeButtons();
@@ -12,89 +18,111 @@ function applyClick(x) {
   var btn = document.getElementById("b" + x);
   var totalArea = document.getElementById("totalArea");
   var currentArea = document.getElementById("currentArea");
-  var equals = false;
+  var speech = document.getElementById("speech");
 
   btn.onclick = () => {
-    if(!isNaN(x)) { //test for number
-      if(currentEntry[0] === undefined || equals){ //clear for first entry
-        equals = false;
+    //first we clear the face
+    speech.style.display = "none";
+    //document.getElementById("face-sleep").style.display = "none";
+    document.getElementById("numberArea").style.display = "block";
+
+       if(equals){ //clear for first entry
+        document.getElementById("face-sleep").style.display = "none";
         currentEntry = [];
         totalEntry = [];
         currentArea.textContent = " ";
         totalArea.textContent = " ";
+
       }
+
+     if(currentEntry.length >= 9 || totalArea.textContent.length >= 17){ //restrict imput length
+      currentArea.textContent = "Number Limit Exceeded!";
+      totalArea.textContent = ":-(";
+      equals = true;
+     }else if(!isNaN(x)) { //test for number
        if(testOperands.test(currentEntry[0])){ //after using operands
-        // console.log("im working");
         currentArea.textContent = x;
         totalArea.textContent = totalArea.textContent + currentEntry[0];
         totalEntry.push(currentEntry[0]);
         currentEntry = [];
         currentEntry.push(x);
       } else { //number behavior:
+        equals = false;
      currentEntry.push(x);
-     //totalEntry.push(x);
      currentArea.textContent = (currentArea.textContent + x);
-    // totalArea.textContent = (totalArea.textContent + x);
-    // console.log(totalEntry + "outside");
    }
-  } else if (isNaN(x)) { //for all non numerics
-        if(testOperands.test(x)){ //for operands
+  } else if (isNaN(x)) { //**for all non numerics**\\
+      if(equals){
+        return;
+      } else {
+        if(testOperands.test(x)){ //for operators
           if(testOperands.test(currentEntry[0])){
-            //if there is ALREADy an operator
-            currentEntry[0] = x;
-            currentArea.textContent = x;
-          } else if (x === "=") {
-            var final = currentEntry.join('');
-            totalEntry.push(final);
-            totalArea.textContent = totalArea.textContent + currentArea.textContent;
-              while(totalEntry.includes("*") || totalEntry.includes("/")){
-                if(totalEntry.includes("*")){
-                  console.log("inside)");
-                  let index = totalEntry.indexOf("*");
-                  let a = totalEntry[index - 1];
-                  let b = totalEntry[index + 1];
-                  let c =  a * b;
-                  //so how to inject into proper place in array?
-                  console.log(index - 1);
-                  console.log(totalEntry);
-                  totalEntry.splice((index - 1),3,c);
-                  //console.log(testArray);
-                  console.log(totalEntry);
-                } else {
-                  let index = totalEntry.indexOf("/");
-                  let a = totalEntry[index - 1];
-                  let b = totalEntry[index + 1];
-                  let c =  a / b;
-                  //injecting new total here
-                  totalEntry.splice((index - 1),3,c);
+            //if there is ALREADy an operator do nothing
+            return;
+          } else if (x === "=") { //to get answer
+            // if(equals || (totalEntry[0] === null)){
+            //   alert("hi");
+            //   return;
+            // } else {
+              speech.style.display = "block";
+              document.getElementById("face-sleep").style.display = "none";
+              document.getElementById("numberArea").style.display = "none";
+              document.getElementById("face-happy").style.display = "block";
+              document.getElementById
+              var final = currentEntry.join('');
+              totalEntry.push(final);
+              totalArea.textContent = totalArea.textContent + currentArea.textContent;
+                while(totalEntry.includes("*") || totalEntry.includes("/")){
+                  if(totalEntry.includes("*")){
+                    console.log("inside)");
+                    let index = totalEntry.indexOf("*");
+                    let a = totalEntry[index - 1];
+                    let b = totalEntry[index + 1];
+                    let c =  a * b;
+                    //so how to inject into proper place in array?
+                    console.log(index - 1);
+                    console.log(totalEntry);
+                    totalEntry.splice((index - 1),3,c);
+                    //console.log(testArray);
+                    console.log(totalEntry);
+                  } else {
+                    let index = totalEntry.indexOf("/");
+                    let a = totalEntry[index - 1];
+                    let b = totalEntry[index + 1];
+                    let c =  a / b;
+                    //injecting new total here
+                    totalEntry.splice((index - 1),3,c);
+                  }
                 }
-              }
-              while(totalEntry.includes("+") || totalEntry.includes("-")){
-                if(totalEntry.includes("+")){
-                  let index = totalEntry.indexOf("+");
-                  let a = Number(totalEntry[index - 1]); //+ is concatenating instead of adding
-                  let b = Number(totalEntry[index + 1]);
-                  let c =  a + b;
+                while(totalEntry.includes("+") || totalEntry.includes("-")){
+                  if(totalEntry.includes("+")){
+                    let index = totalEntry.indexOf("+");
+                    let a = Number(totalEntry[index - 1]); //+ is concatenating instead of adding
+                    let b = Number(totalEntry[index + 1]);
+                    let c =  a + b;
 
-                  totalEntry.splice((index - 1),3,c);
-                } else {
-                  let index = totalEntry.indexOf("-");
-                  let a = totalEntry[index - 1];
-                  let b = totalEntry[index + 1];
-                  let c =  a - b;
-                  //injecting new total here
-                  totalEntry.splice((index - 1),3,c);
+                    totalEntry.splice((index - 1),3,c);
+                  } else {
+                    let index = totalEntry.indexOf("-");
+                    let a = totalEntry[index - 1];
+                    let b = totalEntry[index + 1];
+                    let c =  a - b;
+                    //injecting new total here
+                    totalEntry.splice((index - 1),3,c);
+                  }
                 }
-              }
-            currentEntry = [];
-            equals = true;
-            currentArea.textContent = totalEntry[0];
-            console.log("meowwwwwtside");
-            console.log(totalEntry);
+              currentEntry = [];
+              equals = true;
+              document.getElementById("equation").textContent = totalArea.textContent;
+              document.getElementById("solution").textContent = totalEntry[0];
+              currentArea.textContent = " ";
+              totalArea.textContent = " "
+              //currentArea.textContent = totalEntry[0];
+            // }
           }
           else {
-            var hello = currentEntry.join('') // dude change this var name lmao
-            totalEntry.push(hello); //push the pre-existing numbers into our final
+            var userInput = currentEntry.join('') // dude change this var name lmao
+            totalEntry.push(userInput); //push the pre-existing numbers into our final
             totalArea.textContent = totalArea.textContent + currentArea.textContent;
             currentEntry = []; //clear current entry to make room
             currentEntry.push(x);
@@ -109,25 +137,20 @@ function applyClick(x) {
           }
           //this needs to behave similar to a number but cant use more than one
         } else if (x === "AC" || x === "CE") {
+          document.getElementById('face-happy').style.display = "none";
           currentEntry = [];
-          currentArea.textContent = "0"
+          currentArea.textContent = " "
           if (x === "AC"){
           totalEntry = [];
-          totalArea.textContent = "0";
+          totalArea.textContent = " ";
           }
         } else {
         currentArea.textContent = x;
         totalArea.textContent = (totalArea.textContent + x);
         currentEntry = x;
-      //  totalEntry.push(x + "nonnum");
-        }
+      }
+    }
    }
-   //console.log("current: " + currentEntry);
-   //console.log("total: " + totalEntry);
-   // meow = totalEntry.join(''); didnt expect this to work
-   // totalArea.textContent = meow;
-   // var meow = totalArea.textContent;
-   // console.log("total html: " + meow);
   }
  }
 
@@ -138,24 +161,8 @@ function makeButtons() {
     var container = document.getElementById('container');
     btn.id = "b" + buttons[i];
     btn.className = "button";
-    //btn.onlick = applyClick;
     btn.appendChild(t);
     container.appendChild(btn);
-  //  document.body.appendChild(btn);
     applyClick(buttons[i]);
-
   }
 }
-
-/*
-What you are stuck with is that it needs to store whatever is typed into current entry
-when an operator is used THEn you...:
-take current entry, push to final
-clear current entry and replace with operator
-
-same when using a buttons:
-take current entry (operator) push to final, clear and replace
-
-but what is happening now is you are always pushing to final which is why it isnt working
-
-*/
